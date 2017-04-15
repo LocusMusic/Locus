c8b69a1798771cacec8e6b8c228c86ae27f1136d
Kesong Xie <kesongxie1993@gmail.com>
2017-04-15 10:29:37 -0700

    initial commit with Parse and Spotify iOS SDK set up

---
+++ Podfile
@@ -0,0 +1,4 @@
+use_frameworks!
+target 'Spottunes' do
+	pod 'Parse'
+end
---
+++ Pods/Target Support Files/Pods-Spottunes/Pods-Spottunes-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Spottunes : NSObject
+@end
+@implementation PodsDummy_Pods_Spottunes
+@end
