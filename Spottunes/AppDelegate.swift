cc512749153d9aa7acaf018efe97cebdd16d136c
LeoWW <LeoWW@users.noreply.github.com>
2017-05-03 00:13:34 -0700

    register subclass

--- Spottunes/AppDelegate.swift
+++ Spottunes/AppDelegate.swift
@@ -18,8 +18,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
         // Override point for customization after application launch.
+        
+        TuneSpot.registerSubclass()
+
         SpotifyClient.authInit()
+        
         Parse.initialize(
             with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
                 configuration.applicationId = "SpotTunes"
