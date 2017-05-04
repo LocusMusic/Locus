53e60b6b7fe86b8e8bd0a87e8628fa936941f274
LeoWW <LeoWW@users.noreply.github.com>
2017-05-04 01:23:34 -0700

    implement search view skeleton

---
+++ Spottunes/SearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchTableViewCell.swift
+//  Spottunes
+//
+//  Created by Leo Wong on 5/4/17.
+//  Copyright © 2017 ___Spottunes___. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
