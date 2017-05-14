7f1dc098dbc8e36b8a462ef2d9ad7c85552b400e
LeoWW <LeoWW@users.noreply.github.com>
2017-05-14 10:49:28 -0700

    update artists and playlists

--- Spottunes/Playlists.swift
+++ Spottunes/Playlists.swift
@@ -8,6 +8,44 @@
 
 import UIKit
 
-class Playlists: NSObject {
+fileprivate let HrefKey = "href"
+fileprivate let TotalKey = "total"
+fileprivate let ItemsKey = "items"
+fileprivate let PlaylistKey = "playlist"
+fileprivate let NextKey = "next"
+
+class Playlists {
+    
+    var dict: [String: Any]!
+    
+    init(dict: [String: Any]) {
+        self.dict = dict
+    }
+    
+    var href: String {
+        return self.dict[HrefKey] as! String
+    }
+    
+    var next: String {
+        return self.dict[NextKey] as! String
+    }
+    
+    var total: Int! {
+        return self.dict[TotalKey] as! Int
+    }
+    
+    var playlists: [Playlist]? {
+        guard let itemsDict = self.dict[ItemsKey] as? [[String: Any]] else{
+            return nil
+        }
+        let playlists = itemsDict.map { (itemDict) -> Playlist in
+            if let playlistDict = itemDict[PlaylistKey] as? [String : Any] {
+                return Playlist(dict: playlistDict)
+            } else {
+                return Playlist(dict: itemDict)
+            }
+        }
+        return playlists
+    }
 
 }
