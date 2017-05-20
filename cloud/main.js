9e80799ae3cb09562c87ac76faac01221f93c203
Kesong Xie <kesongxie1993@gmail.com>
2017-05-20 03:26:55 -0700

    update main.js for push notification testing

--- cloud/main.js
+++ cloud/main.js
@@ -1,32 +1,36 @@
 // iOS push testing
 Parse.Cloud.define("iosPushTest", function(request, response) {
-
   // request has 2 parameters: params passed by the client and the authorized user                                                                                                                               
   var params = request.params;
-
   var user = request.user;
-
   // Our "Message" class has a "text" key with the body of the message itself                                                                                                                                    
   var messageText = params.text;
-	
 
-
-  var pushQuery = new Parse.Query(Parse.Installation);
-  pushQuery.equalTo('deviceType', 'ios'); // targeting iOS devices only                                                                                                                                          
-
-	console.log(pushQuery)
-
-  Parse.Push.send({
-    where: pushQuery, // Set our Installation query                                                                                                                                                              
-    data: {
-      alert: "Message: " + messageText
+  var pushQuery = new Parse.Query(Parse.Installation);   
+  var userQuery = new Parse.Query("User");
+  var userQuery.equalTo("spotifyId", "12125995664");
+  
+   
+  userQuery.find({
+    success: function(comments) {
+      response.success('success');
+    },
+    error: function(error) {
+    response.success(error);
+      console.error("Error finding related comments " + error.code + ": " + error.message);
     }
-  }, { success: function() {
-      console.log("#### PUSH OK");
-     response.success('success');
-  }, error: function(error) {
-      console.log("#### PUSH ERROR" + error.message);
-  }, useMasterKey: true});
-
- 
+  }); 
+   
+//   pushQuery.equalTo('deviceType', 'ios'); // targeting iOS devices only                                                                                                                                          
+  // Parse.Push.send({
+//     where: pushQuery, // Set our Installation query                                                                                                                                                              
+//     data: {
+//       alert: "Message: " + messageText
+//     }
+//   }, { success: function() {
+//       console.log("#### PUSH OK");
+//      response.success('success');
+//   }, error: function(error) {
+//       console.log("#### PUSH ERROR" + error.message);
+//   }, useMasterKey: true});
 });
