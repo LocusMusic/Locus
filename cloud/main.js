f68576f298dd7360275530b778f3a0c27417fccb
Kesong Xie <kesongxie1993@gmail.com>
2017-05-20 12:53:46 -0700

    update main.js for push notification testing

--- cloud/main.js
+++ cloud/main.js
@@ -8,7 +8,7 @@ Parse.Cloud.define("iosPushTest", function(request, response) {
   
     var pushQuery = new Parse.Query(Parse.Installation);
 	var userQuery = new Parse.Query(Parse.User);
-	userQuery.equalTo("username", "FaQOWnAhfunO7G5qWvkcjRvzJ")
+	userQuery.equalTo("username", "FaQOWnAhfunO7G5qWvkcjRvzW")
     pushQuery.matchesQuery("user", userQuery);
 //     pushQuery.equalTo('deviceType', 'ios'); // targeting iOS devices only                                                                                                                                          
 
