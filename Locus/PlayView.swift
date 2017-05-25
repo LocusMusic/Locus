75625cd64cdffeb9ceec3f2b0eb4a0a33143babf
LeoWW <LeoWW@users.noreply.github.com>
2017-05-24 22:36:17 -0700

    change heart to up arrow icon on playview

--- Locus/PlayView.swift
+++ Locus/PlayView.swift
@@ -10,7 +10,6 @@ import UIKit
 import Parse
 import AVFoundation
 
-
 fileprivate let xibName = "PlayView"
 
 enum PlayViewUIState{
@@ -53,7 +52,6 @@ class PlayView: UIView {
         }
     }
     
-    
     var currentPlayingState = false{
         didSet{
             if currentPlayingState{
@@ -86,7 +84,19 @@ class PlayView: UIView {
     
     
     @IBAction func favorBtnTapped(_ sender: UIButton) {
-        sender.imageBtnActivateWithColor(color: App.Style.Color.heartActiveColor)
+        //Commented out - For heart button
+        //sender.imageBtnActivateWithColor(color: App.Style.Color.heartActiveColor)
+        self.delegate?.playViewBecomeMaximized(playView: self)
+        let destinationCenterY = 0.5 * App.screenHeight
+        UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 6, options: .curveEaseInOut, animations: {
+            self.center.y = destinationCenterY
+            self.minPlayerView.alpha = 0
+        }, completion: {
+            finished in
+            if finished{
+                self.state = .maximized
+            }
+        })
     }
 
     @IBAction func playToggle(_ sender: UIButton) {
@@ -174,96 +184,96 @@ class PlayView: UIView {
     
     
     
-    func playViewPanning(_ gesture: UIPanGestureRecognizer){
+    func playViewPanning(_ gesture: UIPanGestureRecognizer) {
+        print("PlayViewPlanning Called!!")
         switch self.state{
-        case .minimized:
-            let translation = gesture.translation(in: nil)
-            let destinationCenterY = 0.5 * App.screenHeight
-            switch gesture.state{
-            case .began:
-                self.minimizedCenter = self.center
-                self.originalCenter = self.center
-            case .changed:
-                let newCenterY = max(min(self.originalCenter.y, self.originalCenter.y + translation.y), destinationCenterY)
-                self.delta = abs((newCenterY - destinationCenterY)) / (self.originalCenter.y - destinationCenterY) //maps from 1 to 0
-                self.minPlayerView.alpha = self.delta
-                self.center.y = newCenterY
-                self.delegate?.panning(playView: self, delta: self.delta)
-            case .ended:
-                if self.delta < 0.8{
-                    //set to open position
-                    self.delegate?.playViewBecomeMaximized(playView: self)
-                    UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 6, options: .curveEaseInOut, animations: {
-                        self.center.y = destinationCenterY
-                        self.minPlayerView.alpha = 0
-                    }, completion: {
-                        finished in
-                        if finished{
-                            self.state = .maximized
+            case .minimized:
+                let translation = gesture.translation(in: nil)
+                let destinationCenterY = 0.5 * App.screenHeight
+                switch gesture.state {
+                    case .began:
+                        self.minimizedCenter = self.center
+                        self.originalCenter = self.center
+                    case .changed:
+                        let newCenterY = max(min(self.originalCenter.y, self.originalCenter.y + translation.y), destinationCenterY)
+                        self.delta = abs((newCenterY - destinationCenterY)) / (self.originalCenter.y - destinationCenterY) //maps from 1 to 0
+                        self.minPlayerView.alpha = self.delta
+                        self.center.y = newCenterY
+                        self.delegate?.panning(playView: self, delta: self.delta)
+                    case .ended:
+                        if self.delta < 0.8{
+                            //set to open position
+                            self.delegate?.playViewBecomeMaximized(playView: self)
+                            UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 6, options: .curveEaseInOut, animations: {
+                                self.center.y = destinationCenterY
+                                self.minPlayerView.alpha = 0
+                            }, completion: {
+                                finished in
+                                if finished{
+                                    self.state = .maximized
+                                }
+                            })
+                        }else{
+                            self.delegate?.playViewBecomeMinimized(playView: self)
+                            UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 6, options: .curveEaseInOut, animations: {
+                                self.center.y = self.originalCenter.y
+                                self.minPlayerView.alpha = 1
+                            }, completion: {
+                                finished in
+                                if finished{
+                                    self.state = .minimized
+                                }
+                            })
                         }
-                    })
-                }else{
-                    self.delegate?.playViewBecomeMinimized(playView: self)
-                    UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 6, options: .curveEaseInOut, animations: {
-                        self.center.y = self.originalCenter.y
-                        self.minPlayerView.alpha = 1
-                    }, completion: {
-                        finished in
-                        if finished{
-                            self.state = .minimized
+                    default:
+                        break
+                }
+            case .maximized:
+                let translation = gesture.translation(in: nil)
+                let destinationCenterY = self.minimizedCenter.y
+                switch gesture.state{
+                    case .began:
+                        self.maximizedCenter = self.center
+                        self.originalCenter = self.center
+                    case .changed:
+                        let newCenterY = max(self.maximizedCenter.y, self.originalCenter.y + translation.y)
+                        self.delta = 1 - abs((newCenterY - destinationCenterY)) / (destinationCenterY - self.originalCenter.y) //maps from 0 to 1 //change
+                        self.minPlayerView.alpha = self.delta
+                        self.center.y = newCenterY
+                        self.delegate?.panning(playView: self, delta: self.delta)
+                    case .ended:
+                        print(self.delta)
+                        if self.delta < 0.2{
+                            //set to open position
+                            self.delegate?.playViewBecomeMaximized(playView: self)
+
+                            UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 6, options: .curveEaseInOut, animations: {
+                                self.center.y = self.originalCenter.y
+                                self.minPlayerView.alpha = 0
+                            }, completion: {
+                                finished in
+                                if finished{
+                                    self.state = .maximized
+                                }
+                            })
+                        } else {
+                            self.delegate?.playViewBecomeMinimized(playView: self)
+                            UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 6, options: .curveEaseInOut, animations: {
+                                self.center.y = destinationCenterY
+                                self.minPlayerView.alpha = 1
+                            }, completion: {
+                                finished in
+                                if finished{
+                                    self.state = .minimized
+                                }
+                            })
                         }
-                    })
+                    default:
+                        break
                 }
             default:
                 break
             }
-        case .maximized:
-            let translation = gesture.translation(in: nil)
-            let destinationCenterY = self.minimizedCenter.y
-            switch gesture.state{
-            case .began:
-                self.maximizedCenter = self.center
-                self.originalCenter = self.center
-            case .changed:
-                let newCenterY = max(self.maximizedCenter.y, self.originalCenter.y + translation.y)
-                self.delta = 1 - abs((newCenterY - destinationCenterY)) / (destinationCenterY - self.originalCenter.y) //maps from 0 to 1 //change
-                self.minPlayerView.alpha = self.delta
-                self.center.y = newCenterY
-                self.delegate?.panning(playView: self, delta: self.delta)
-            case .ended:
-                print(self.delta)
-                if self.delta < 0.2{
-                    //set to open position
-                    self.delegate?.playViewBecomeMaximized(playView: self)
-
-                    UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 6, options: .curveEaseInOut, animations: {
-                        self.center.y = self.originalCenter.y
-                        self.minPlayerView.alpha = 0
-                    }, completion: {
-                        finished in
-                        if finished{
-                            self.state = .maximized
-                        }
-                    })
-                }else{
-                    self.delegate?.playViewBecomeMinimized(playView: self)
-                    UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 6, options: .curveEaseInOut, animations: {
-                        self.center.y = destinationCenterY
-                        self.minPlayerView.alpha = 1
-                    }, completion: {
-                        finished in
-                        if finished{
-                            self.state = .minimized
-                        }
-                    })
-                }
-            default:
-                break
-            }
-
-        default:
-            break
-        }
     }
     
     func playActiveTrack(){
